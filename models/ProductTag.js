64c88574db4f10ff0f6d9ba691410c75b991b950
jcerb <brt.jst@gmail.com>
2021-03-31 18:54:11 -0700

    set product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,9 +4,21 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+ // define columns
 ProductTag.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+      },
+    },
   },
   {
     sequelize,
