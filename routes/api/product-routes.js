1674a5d5a73b1c1d0a176e673c1a59a4ffcd5168
jcerb <brt.jst@gmail.com>
2021-04-01 18:38:46 -0700

    setup product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,28 +3,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
+// find all products
   // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Category.findAll({
+    include: [{model: Category}, {model: Product},{model: Tag},{model: ProductTag}],
+  });
+  res.status(200).json(productData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+// find a single product by its `id`
   // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try{
+    const productData = await Category.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Product},{model: Tag},{model: ProductTag}],
+    });
+    if(!productData){
+      res.status(404).json({message: "No category found with that #id"});
+      return;
+    }
+  }catch (err){
+    res.status(500).json(err);
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+ 
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -94,3 +104,12 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+ /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
